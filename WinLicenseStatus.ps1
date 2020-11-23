8489cba7c9f01f137d4d9d9516f56404e20d9f1b
khanagha Hashemi <57272705+khanhashemi@users.noreply.github.com>
2020-11-23 17:43:03 +0100

    Update WinLicenseStatus.ps1

--- WinLicenseStatus.ps1
+++ WinLicenseStatus.ps1
@@ -1,3 +1,11 @@
+# **************************************************************
+# 
+#  
+# Author: Khanagha Hashemi
+#       
+#     
+# **************************************************************
+
 $WinLicenseStatus = Get-CimInstance -ClassName SoftwareLicensingProduct  -Filter "PartialProductKey IS NOT NULL" | Where-Object Name -like "Windows*" 
 $Status = if ($WinLicenseStatus.LicenseStatus -eq 1) { "Activated" } else { "Not Activated" }
 $Status 
